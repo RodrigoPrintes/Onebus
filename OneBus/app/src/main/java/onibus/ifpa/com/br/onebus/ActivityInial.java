8b1044e846aa02245fa9663c5bc60ff29eb96fb2
RodrigoPrintes <drigoprintes00@gmail.com>
2017-04-11 16:44:54 -0300

    primeiro commit

---
+++ OneBus/app/src/main/java/onibus/ifpa/com/br/onebus/ActivityInial.java
@@ -0,0 +1,13 @@
+package onibus.ifpa.com.br.onebus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityInial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityinial);
+    }
+}
